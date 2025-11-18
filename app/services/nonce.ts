2e8772b15d39f81ea7ff18b92504330a0482af28
JoSuzuki <jonathan@suzuki.pro.br>
2025-11-17 22:28:28 -0300

    nonce

---
+++ app/services/nonce.ts
@@ -0,0 +1,6 @@
+import { createContext, useContext } from 'react'
+
+export const NonceContext = createContext<string | undefined>(undefined)
+
+export const useNonce = () => useContext(NonceContext)
+
